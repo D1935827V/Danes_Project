95f3a2e29b35a5c8a162a143334c27e02c142194
D1935827V <dvestal2102@gmail.com>
2020-10-20 09:50:20 -0700

    web

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+@app.route('/cakes')
+def cakes():
+    return 'yummy cakes'
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 3000)
